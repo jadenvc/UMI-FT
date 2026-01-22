66d0e5f75bb5af0d943120573632450759f7a8e8
Hojung Choi <ilovehjb0520@gmail.com>
2026-01-21 19:46:15 -0800

    file cleanups

--- umi_day/umi_day/__init__.py
+++
@@ -1,3 +0,0 @@
-from .common.generic_util import register_omegeconf_resolvers
-
-register_omegeconf_resolvers()
--- umi_day/umi_day/pose_estimation/util/mocap/README.md
+++
@@ -1,1 +0,0 @@
-Motion capture utilities are adapted from https://github.com/real-stanford/umi-on-legs.
